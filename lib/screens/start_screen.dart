e6e2beb8c2feb559b9a39b72ab578338b2fe5840
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2023-08-18 00:40:46 -0600

    avengers

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -33,6 +33,7 @@ class _StartScreenState extends State<StartScreen> {
                   SizedBox(width: 20,),
                   SuggestionCard( img: 'assets/img/pianista.jpg', url: 'https://youtu.be/yDA1mK6v-ME', titulo: 'El pianista',),
                   SuggestionCard( img: 'assets/img/trasnformers.jpeg', url: 'https://youtu.be/v0d0id78XdE', titulo: 'Tranformers',),
+                  SuggestionCard( img: 'assets/img/avengers.jpg', url: 'https://youtu.be/yNXfOOL8824', titulo: 'Avengers',),
                 ],
               ),
         ),
